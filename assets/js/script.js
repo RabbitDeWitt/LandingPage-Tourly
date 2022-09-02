64bcf7640562b9cce7c5806a0f53497bf0bc68fa
David Coelho <davidsocoelho@outlook.com>
2022-09-02 05:43:46 -0300

    Mobile completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@ const overlay = document.querySelector('[data-overlay]')
 const navOpenBtn = document.querySelector('[data-open-menu-btn]')
 const navCloseBtn = document.querySelector('[data-close-menu-btn]')
 const navbar = document.querySelector('[data-navbar]')
-const navLinks = document.querySelector('[data-navbar-link]')
+const navLinks = document.getElementsByClassName('navbar-link')
 
 const navElemArr = [navOpenBtn, navCloseBtn, overlay]
 
 const navToggleEvent = (elem) => {
+  console.log(navLinks)
   for(let i = 0; i < elem.length; i++){
     elem[i].addEventListener('click', () =>{
       navbar.classList.toggle("active")
@@ -15,16 +16,19 @@ const navToggleEvent = (elem) => {
   }
 }
 
-navToggleEvent(navElemArr)
 navToggleEvent(navLinks)
+navToggleEvent(navElemArr)
 
 
 const header = document.querySelector('[data-header]')
+const goToTopBtn = document.querySelector('[data-go-top]')
 
 window.addEventListener('scroll', () => {
-  if(window.scrollY >= 124){
+  if(window.scrollY >= 100){
     header.classList.add("active")
+    goToTopBtn.classList.add("active")
   }else{
     header.classList.remove("active")
+    goToTopBtn.classList.remove("active")
   }
 })
